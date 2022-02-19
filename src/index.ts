d032059c5f451fca7eb35f7b97ff747e1a407a23
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-20 00:36:36 +0300

    refactor: add API routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import usersRoutes from "./atoms/users/router";
+import subjectsRoutes from "./atoms/subjects/router";
+import roomsRoutes from "./atoms/rooms/router";
+import lecturesRoutes from "./atoms/lectures/router";
 
 const router = Router();
 
 router.use("/api/users", usersRoutes);
+
+router.use("/api/subjects", subjectsRoutes);
+
+router.use("/api/rooms", roomsRoutes);
+
+router.use("/api/lectures", lecturesRoutes);
 
 export default router;
