6c3f3fe684bc442461a2b62667039acd0fdb28d6
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-23 00:21:01 +0300

    Add schedule route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import usersRoutes from "./atoms/users/router";
 import subjectsRoutes from "./atoms/subjects/router";
 import roomsRoutes from "./atoms/rooms/router";
 import lecturesRoutes from "./atoms/lectures/router";
+import schedulesRoutes from "./atoms/schedules/router";
 
 const router = Router();
 
@@ -13,5 +14,7 @@ router.use("/api/subjects", subjectsRoutes);
 router.use("/api/rooms", roomsRoutes);
 
 router.use("/api/lectures", lecturesRoutes);
+
+router.use("/api/schedules", schedulesRoutes);
 
 export default router;
