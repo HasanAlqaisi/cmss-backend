7219c42f9a3c04b80d88bfc0d899d9b0a80166f3
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-23 19:34:21 +0300

    Setup swagger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 import passport from "passport";
+import swaggerUi from "swagger-ui-express";
 import { NotFoundError } from "./utils/api/api-error";
 import jwtConfig from "./utils/auth/jwt";
 import logger from "./utils/config/logger";
 import "./utils/config/general";
 import mainErrorHandler from "./utils/main-error-handler";
 import mainRoute from "./index";
+import specs from "../openapi.json";
 
 const app: Application = express();
 
@@ -22,6 +24,11 @@ app.use(morgan("dev"));
 // Configure passport for auth
 app.use(passport.initialize());
 jwtConfig(passport);
+
+if (process.env.NODE_ENV === "development") {
+  app.use("/docs", swaggerUi.serve);
+  app.get("/docs", swaggerUi.setup(specs));
+}
 
 // Main app route
 app.use("/", mainRoute);
