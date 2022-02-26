f4dd6bba8ff64a3d94ab93ea991a7a11532be6b6
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-26 23:16:02 +0300

    Add conditions to ability

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,7 +1,9 @@
 import { AbilityBuilder, AbilityClass } from "@casl/ability";
 import { PrismaAbility } from "@casl/prisma";
 import { Prisma, User } from "@prisma/client";
+import Mustache from "mustache";
 import { UserWithPermissions } from "../atoms/users/types";
+import logger from "./config/logger";
 
 type AppAbilityType = PrismaAbility<[string, Prisma.ModelName | "all"]>;
 
@@ -11,9 +13,19 @@ const { can, build } = new AbilityBuilder(AppAbility);
 
 export default (user?: UserWithPermissions) => {
   user?.role.permissions.forEach((permission) => {
-    // const conditionsObject = permission.conditions as Prisma.JsonObject;
-    // can(permission.action, permission.subject, conditionsObject);
-    can(permission.action, permission.subject);
+    // Reading conditions object
+    const conditionsObject = permission.conditions as Prisma.JsonObject;
+
+    if (conditionsObject) {
+      // replacing id placeholder with the actual one
+      const stringId = Mustache.render(conditionsObject.id as any, {
+        id: user.id,
+      });
+
+      conditionsObject.id = Number(stringId);
+    }
+
+    can(permission.action, permission.subject, conditionsObject as any);
   });
   return build();
 };
