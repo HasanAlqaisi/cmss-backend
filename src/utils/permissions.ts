8c1b077854a5713f47bbc8e23d1b320803bf0ef0
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-26 20:31:00 +0300

    Remove unused import

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -2,7 +2,6 @@ import { AbilityBuilder, AbilityClass } from "@casl/ability";
 import { PrismaAbility } from "@casl/prisma";
 import { Prisma, User } from "@prisma/client";
 import { UserWithPermissions } from "../atoms/users/types";
-import logger from "./config/logger";
 
 type AppAbilityType = PrismaAbility<[string, Prisma.ModelName | "all"]>;
 
@@ -11,11 +10,9 @@ const AppAbility = PrismaAbility as AbilityClass<AppAbilityType>;
 const { can, build } = new AbilityBuilder(AppAbility);
 
 export default (user?: UserWithPermissions) => {
-  logger.debug(user?.email);
   user?.role.permissions.forEach((permission) => {
     // const conditionsObject = permission.conditions as Prisma.JsonObject;
     // can(permission.action, permission.subject, conditionsObject);
-    logger.debug(`${permission.action}, ${permission.subject}`);
     can(permission.action, permission.subject);
   });
   return build();
