00f62bc3d4c5ce1b3111db407c615bbeb638e882
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-02-27 22:38:52 +0300

    Return user with his permissions when checking auth

--- src/utils/auth/jwt.ts
+++ src/utils/auth/jwt.ts
@@ -1,5 +1,6 @@
 import { PassportStatic } from "passport";
 import { ExtractJwt, StrategyOptions } from "passport-jwt";
+import UserService from "../../atoms/users/service";
 import prisma from "../../prisma";
 
 const JwtStrategy = require("passport-jwt").Strategy;
@@ -13,9 +14,7 @@ export default function jwtConfig(passport: PassportStatic) {
   passport.use(
     new JwtStrategy(jwtOptions, async (payload: any, done: any) => {
       try {
-        const user = await prisma.user.findUnique({
-          where: { id: payload.id },
-        });
+        const user = await UserService.findById(payload.id);
         if (user) return done(null, user);
 
         return done(null, false);
