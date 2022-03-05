4451099f411ad4911a46653cbf09655ddaf7f87e
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-03-05 15:25:18 +0300

    refactor: :art: validation of specialty fields

--- src/atoms/specialties/validator.ts
+++ src/atoms/specialties/validator.ts
@@ -3,25 +3,25 @@ import { z } from "zod";
 
 // eslint-disable-next-line import/prefer-default-export
 export const upsertSpecialty = async (req: Request) => {
-  const schema = z
-    .object({
-      name: z.string(),
-      minAvg: z.number().positive(),
-      isDependent: z.boolean().default(false),
-      customPercentage: z.number().default(0),
-    })
-    .refine(
-      (data) => {
-        const condition1 = data.isDependent && data.customPercentage;
-        const condition2 = !data.isDependent;
+  const mainFields = z.object({
+    name: z.string(),
+    minAvg: z.number().positive(),
+  });
 
-        return condition1 || condition2;
-      },
-      {
-        path: ["customPercentage"],
-        message: "You should enter customPercentage",
-      }
-    );
+  const independentSpecialty = z
+    .object({
+      isDependent: z.literal(false),
+    })
+    .and(mainFields);
+
+  const dependentSpecialty = z
+    .object({
+      isDependent: z.literal(true),
+      customPercentage: z.number().positive(),
+    })
+    .and(mainFields);
+
+  const schema = independentSpecialty.or(dependentSpecialty);
 
   return schema.parseAsync(req.body);
 };
