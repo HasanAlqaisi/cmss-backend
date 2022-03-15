d050375f6ee20547183ce6236d5045b7d4dd702c
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-03-15 15:54:34 +0300

    Return number of conflicts with the resulted schedule

--- src/atoms/schedules/service.ts
+++ src/atoms/schedules/service.ts
@@ -1,33 +1,21 @@
 import { Subject } from "@prisma/client";
 import prisma from "../../prisma";
 import { Chromosome, Gene } from "./types";
-import createNextGeneration from "../../algorithms/genetic-algorithm/create-next-generation";
-import computeFitness from "../../algorithms/genetic-algorithm/fitness/compute-fitness";
-import initPopulation from "../../algorithms/genetic-algorithm/init-population";
-import writeChromosomesToFile from "../../algorithms/genetic-algorithm/write-chromosomes-to-file";
-import { maxGeneration, populationSize } from "../../algorithms/genetic-algorithm/constants";
-import logger from "../../utils/config/logger";
-import LectureService from "../lectures/service";
 import generateSchedule from "../../algorithms/genetic-algorithm/generate-schedule";
 
 export default class ScheduleService {
   static createSchedule = async () => {
     await this.deleteSchedule();
-    const generatedSchedules = await generateSchedule();
-    // if (generatedSchedules.fitness === 1) {
-    //   await prisma.schedule.createMany({
-    //     data: generatedSchedules.genes,
-    //   });
-    //   response = await this.getSchedules();
-    // } else {
-    //   response = "couldn't find a good schedule, try again!";
-    // }
+
+    const generatedSchedule = await generateSchedule();
 
     await prisma.schedule.createMany({
-      data: generatedSchedules.genes,
+      data: generatedSchedule!.genes,
     });
-    const response = await this.getSchedules();
-    return response;
+
+    const schedule = await this.getSchedules();
+
+    return { conflicts: 1 / generatedSchedule.fitness, schedule };
   };
 
   static deleteSchedule = async () => {
