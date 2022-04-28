7ebe4f8e2ef908203c2599854bbebf677f2e008c
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-04-29 00:52:13 +0300

    fix: logic of check requester info

--- src/middlewares/check-requester.ts
+++ src/middlewares/check-requester.ts
@@ -1,14 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import UserService from "../atoms/users/service";
+import { UserWithPermissions } from "../atoms/users/types";
 import { ForbiddenError } from "../utils/api/api-error";
+import logger from "../utils/config/logger";
 import getIdFromPayload from "../utils/get-id-from-payload";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  const requesterId = getIdFromPayload(req.params.id);
+  const requesterId = getIdFromPayload(req.headers.authorization!);
 
   const user = await UserService.findById(requesterId);
 
-  if (req.user !== user) {
+  const userRequester = req.user as UserWithPermissions;
+
+  if (userRequester.role.name !== "root" && req.user !== user) {
     throw new ForbiddenError();
   }
 
