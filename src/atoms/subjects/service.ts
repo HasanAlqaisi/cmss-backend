ceecf96d822f64c740b01475f659e90ca59ef117
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-04-29 22:49:22 +0300

    include class info in subjects response

--- src/atoms/subjects/service.ts
+++ src/atoms/subjects/service.ts
@@ -3,12 +3,21 @@ import prisma from "../../prisma";
 
 export default class SubjectService {
   static getSubjects = async (): Promise<Subject[]> => {
-    const subjects = await prisma.subject.findMany();
+    const subjects = await prisma.subject.findMany({
+      include: {
+        Class: { include: { branch: true, program: true, stage: true } },
+      },
+    });
     return subjects;
   };
 
   static getSubjectsForClass = async (classId: number): Promise<Subject[]> => {
-    const subjects = await prisma.subject.findMany({ where: { classId } });
+    const subjects = await prisma.subject.findMany({
+      where: { classId },
+      include: {
+        Class: { include: { branch: true, program: true, stage: true } },
+      },
+    });
     return subjects;
   };
 
@@ -19,6 +28,9 @@ export default class SubjectService {
     isLab: boolean
   ): Promise<Subject> => {
     const subject = await prisma.subject.create({
+      include: {
+        Class: { include: { branch: true, program: true, stage: true } },
+      },
       data: {
         classId,
         name,
@@ -43,6 +55,9 @@ export default class SubjectService {
   ): Promise<Subject> => {
     const subject = await prisma.subject.update({
       where: { id },
+      include: {
+        Class: { include: { branch: true, program: true, stage: true } },
+      },
       data: { classId, name, isElectronic, isLab },
     });
 
